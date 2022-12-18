2c37ea5ca6015a45f498b9faa1664b6c8557cabf
IbrahimBadr <hamedb.ibrahim@gmail.com>
2022-12-18 14:28:58 +0200

    Lab done

--- Ibrahim Badr - Lab1/main.cpp
+++ Ibrahim Badr - Lab1/main.cpp
@@ -4,46 +4,84 @@ using namespace std;
 
 class Complex
 {
-    float real;
-    float imag;
+
+    float real, imag;
+
 public:
-    void setReal(float r);
+
+    void setReal(float r)
     {
         real=r;
     }
+
     void setImag(float i)
     {
         imag= i;
     }
+
     float getReal()
     {
         return real;
     }
+
     float getImag()
     {
         return imag;
     }
+
     void print()
     {
         if (0>imag)
         {
-            cout<<real<<" - "<<fabs(imag)<<"i"<<endl;
+            cout<<real<<imag<<"i"<<endl;
         }
         else
         {
-            cout<<real<<" + "<<fabs(imag)<<"i"<<endl;
+            cout<<real<<"+"<<imag<<"i"<<endl;
         }
+    }
+
+    void add(Complex temp1, Complex temp2)
+    {
+        real=temp1.getReal()+temp2.getReal();
+        imag=temp1.getImag()+temp2.getImag();
+    }
+
+    void sub(Complex temp1, Complex temp2)
+    {
+        real=temp1.getReal()-temp2.getReal();
+        imag=temp1.getImag()-temp2.getImag();
     }
 };
 
 
 int main()
 {
-    clrscr();
     Complex myComp1, myComp2, resultComp;
-    myComp1.setReal(7);
-    myComp1.setImag(2);
-    myComp2.setReal(5);
-    myComp2.setImag(3);
+    float rinp,iinp;
+
+    cout<<"Complex No. 1: Enter the Real part then Enter the Imaginary part"<<endl;
+    cin>>rinp>>iinp;
+
+    myComp1.setReal(rinp);
+    myComp1.setImag(iinp);
+    myComp1.print();
+    cout<<endl;
+
+    cout<<"Complex No. 2: Enter the Real part then Enter the Imaginary part"<<endl;
+    cin>>rinp>>iinp;
+
+    myComp2.setReal(rinp);
+    myComp2.setImag(iinp);
+    myComp2.print();
+    cout<<endl;
+
+    resultComp.add(myComp1,myComp2);
+    resultComp.print();
+    cout<<endl;
+
+    resultComp.sub(myComp1,myComp2);
+    resultComp.print();
+
     return 0;
 }
