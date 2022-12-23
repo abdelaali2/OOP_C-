339180ce646e9dd41fde05c301d258a9856a556c
IbrahimBadr <hamedb.ibrahim@gmail.com>
2022-12-23 02:25:51 +0200

    Bonus 2 Done

--- Ibrahim Badr - Lab2/Lab2 - Stack/main.cpp
+++ Ibrahim Badr - Lab2/Lab2 - Stack/main.cpp
@@ -16,7 +16,7 @@ public:
     {
         st=new int[n];
         Size=n;
-        tos=-1;
+        tos=0;
         counter++;
         cout<<"Stack Constructor: This is Stack object No. "<<counter<<endl;
     }
@@ -37,9 +37,8 @@ public:
 
     int pop(int a)
     {
-        if (ifexists(a))
+        if (ifexisits(a))
         {
-
             int popped;
             if (0==tos)
             {
@@ -56,15 +55,22 @@ public:
         }
         else
         {
-                cout<<"INVALID ELEMENT!"<<endl;
+            cout<<"INVALID ELEMENT!"<<endl;
         }
     }
 
     void print()
     {
-        for (int i=0; i<=tos; i++)
+        if (0==tos)
         {
-            cout <<st[i]<<endl;
+            cout<<"STACK EMPTY!"<<endl;
+        }
+        else
+        {
+            for (int i=0; i<=tos; i++)
+            {
+                cout<<st[i]<<endl;
+            }
         }
     }
 
@@ -75,52 +81,78 @@ public:
         cout<<"Stack Destructor: Stack object No. "<<counter+1<<" will be terminated."<<endl;
     }
 
-    bool ifexists (int a)
+    bool ifexisits (int a)
     {
+        cout<<"ifexisits is here";
         for (int i=0; i<=Size; i++)
         {
             if (st[i]==a)
+            {
                 return true;
+            }
         }
         return false;
     }
-
 
 };
 
 int Stack::counter=0; // Initialization
 
+void menu(Stack &);
+
 int main()
 {
-    // Bonus No.1: Create the stack with a user input size.
-
     int length;
-    int User_input;
-
+    // Bonus No.1: Create the stack with a user input size.
     cout<<"Enter the size of the Stack"<<endl;
     cin>>length;
     Stack s1(length);
-
-    for (int i=0; i<length; i++)
-    {
-        cout<<"Enter value No. "<<i+1<<endl;
-        cin>>User_input;
-        s1.push(User_input);
-    }
-
-    s1.print();
-
-    cout<<"Enter the value you want to pop."<<endl;
-    cin>>User_input;
-
-    s1.pop(User_input);
-    s1.print();
-
+    menu(s1);
     return 0;
 }
 
 
+//Bonus No.2: Menu
 
 
+void menu(Stack &s1)
+{
+    int choice,User_input,cont,exit=0;
 
+    while (0==exit)
+    {
+        cout<<endl<<endl;
+        cout<<"Press 1 to Display the Stack"<<endl;
+        cout<<"Press 2 to Push into the Stack"<<endl;
+        cout<<"Press 3 to Pop from the Stack"<<endl;
+        cout<<"Press any key to exit"<<endl;
+
+        cin>>choice;
+
+        switch (choice)
+        {
+
+        case 1:
+            s1.print();
+
+            break;
+
+        case 2:
+            cout<<"Enter value"<<endl;
+            cin>>User_input;
+            s1.push(User_input);
+
+            break;
+        case 3:
+            cout<<"Enter the value you want to pop."<<endl;
+            cin>>User_input;
+            s1.pop(User_input);
+            break;
+        default:
+            exit=1;
+            break;
+        }
+    }
+    return;
+}
 
